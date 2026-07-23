9dc275cb05c8b27f82b90fdf7c514054dc490b07
KaushalTathod <kaushaltathod30@gmail.com>
2026-07-24 02:08:19 +0530

    after fix

--- router/admin.js
+++ router/admin.js
@@ -167,8 +167,9 @@ router.post('/forget', otpLimiter, async (req, res) => {
             // Gmail SMTP Setup
             var transporter = nodemailer.createTransport({
                 host: "smtp.gmail.com",
-                port: 465,
-                secure: true,
+                port: 587,
+                secure: false,
+                family: 4,
                 auth: {
                     user: process.env.EMAIL_USER,
                     pass: process.env.EMAIL_PASS
